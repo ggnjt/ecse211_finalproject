ff06754f1f38520d7aabe76c9ae3f020543f026e
Elias Al Homsi <elias.alhomsi@mail.mcgill.ca>
2019-11-10 03:36:32 -0500

    fix everything elias is god and add wifi package

--- src/ca/mcgill/ecse211/finalproject/phase2/ColorPoller.java
+++ src/ca/mcgill/ecse211/finalproject/phase2/ColorPoller.java
@@ -1,14 +1,15 @@
 package ca.mcgill.ecse211.finalproject.phase2;
 
+import static ca.mcgill.ecse211.finalproject.Resources.FORWARD_SPEED;
 import static ca.mcgill.ecse211.finalproject.Resources.TILE_SIZE;
 import static ca.mcgill.ecse211.finalproject.Resources.leftColorSensor;
+import static ca.mcgill.ecse211.finalproject.Resources.leftMotor;
 import static ca.mcgill.ecse211.finalproject.Resources.navigation;
 import static ca.mcgill.ecse211.finalproject.Resources.odometer;
 import static ca.mcgill.ecse211.finalproject.Resources.rightColorSensor;
+import static ca.mcgill.ecse211.finalproject.Resources.rightMotor;
 import ca.mcgill.ecse211.finalproject.Navigation.TravelingMode;
 import ca.mcgill.ecse211.finalproject.Resources;
-import lejos.hardware.Sound;
-import lejos.robotics.SampleProvider;
 
 /**
  * Class which takes care of color sensor polling, signal filtering as well as odometry correction using the color
@@ -18,124 +19,105 @@ import lejos.robotics.SampleProvider;
  *
  */
 public class ColorPoller implements Runnable {
-  // =====left sensor=====//
-  /**
-   * sample provider for the left color sensor
-   */
-  private static SampleProvider leftSampleProvider = leftColorSensor.getRedMode();
-  /**
-   * sample reading from the left color sensor
-   */
-  private static float[] leftSampleColor = new float[leftColorSensor.sampleSize()];
-  /**
-   * value from left sensor
-   */
-  private static float leftSample;
-  /**
-   * change in sensor measurement between two iterations of the left color sensor sampling
-   */
-  private static float leftDer = 0;
-  /**
-   * used to hold previous value of the left sampler
-   */
-  private static float leftPrev = 0;
 
-  // =====right sensor=====//
-  /**
-   * sample provider for the right color sensor
-   */
-  private static SampleProvider rightSampleProvider = rightColorSensor.getRedMode();
-  /**
-   * sample reading from the right color sensor
-   */
-  private static float[] rightSampleColor = new float[rightColorSensor.sampleSize()];
-  /**
-   * value from right sensor
-   */
-  private static float rightSample;
-  /**
-   * change in sensor measurement between two iterations of the right color sensor sampling
-   */
-  private static volatile float rightDer = 0f;
-  /**
-   * used to hold previous value of the right sampler
-   */
-  private static volatile float rightPrev = 0f;
 
-  /**
-   * A threshold value used to determine a large enough change in sensor measurement
-   */
-  private static final float SENSOR_DERIVATIVE_THRESHOLD = 0.05f;
-
-  /**
-   * a global kill switch for the poller thread
-   */
-  public static boolean kill = false;
   /**
    * whether or not the left sensor is currently detecting a black line
    */
-  public static volatile boolean leftLineDetected;
+  private boolean leftLineDetected = false;
   /**
    * whether or not the right sensor is currently detecting a black line
    */
-  public static volatile boolean rightLineDetected;
-  /**
-   * flag indicating whether the robot is in the process of odometry correction
-   */
-  public static volatile boolean isCorrecting = false;
+  private boolean rightLineDetected = false;
 
+  /**
+   * make the thread wait while turning
+   */
   public static boolean wait = false;
 
-  private void readLeft() {
-    leftSampleProvider.fetchSample(leftSampleColor, 0);
-    leftSample = leftSampleColor[0];
-    leftDer = leftSample - leftPrev;
+  /**
+   * leftSampler
+   */
+  ColorSampler leftSampler;
+
+  /**
+   * Right Sampler
+   */
+  ColorSampler rightSampler;
+
+  /**
+   * leftColorThread
+   */
+  Thread leftColorThread;
+
+  /**
+   * RightColorThread
+   */
+  Thread rightColorThread;
+
+  public ColorPoller() {
+    super();
+    leftSampler = new ColorSampler(leftColorSensor);
+    rightSampler = new ColorSampler(rightColorSensor);
+
+    leftColorThread = new Thread(leftSampler);
+    rightColorThread = new Thread(rightSampler);
+
+    leftColorThread.start();
+    rightColorThread.start();
   }
-  
-  private void readRight() {
-    rightSampleProvider.fetchSample(rightSampleColor, 0);
-    rightSample = rightSampleColor[0];
-    rightDer = rightSample - rightPrev;
-  }
+
   /**
    * run method for the two color poller
    */
+  @Override
   public void run() {
-    long readingStart, readingEnd;
-    while (!kill) {
+    while (true) {
       if (wait) {
         try {
-          Thread.sleep(60);
+          Thread.sleep(30);
         } catch (InterruptedException e) {
           e.printStackTrace();
         }
       } else {
+        long readingStart, readingEnd;
         readingStart = System.currentTimeMillis();
-       
-        if(!leftLineDetected) readLeft();
-        if(!rightLineDetected) readRight();
-        
-        if (navigation.navigationMode == TravelingMode.TRAVELING
-            || navigation.navigationMode == TravelingMode.OBSTACLE_ENCOUNTERED) {
-          leftLineDetected = leftDetectBlackLine();
-          rightLineDetected = rightDetectBlackLine();
-          if (leftLineDetected || rightLineDetected) {
-            navigation.navigationMode = TravelingMode.CORRECTING;
-            navigation.setSpeed(40);
-            synchronized (navigation) {
-              navigation.interrupted = true;
+
+        switch (navigation.navigationMode) {
+          case TRAVELING:
+          case OBSTACLE_ENCOUNTERED:
+            leftLineDetected = leftSampler.getBlackLine();
+            rightLineDetected = rightSampler.getBlackLine();
+            if (leftLineDetected || rightLineDetected) {
+              navigation.navigationMode = TravelingMode.CORRECTING;
+              navigation.setSpeed(40);
+              navigation.moveSuccessful = false;
+              navigation.stopTheRobot();
             }
-          }
-        } else if (navigation.navigationMode == TravelingMode.CORRECTING) {
-          if (!leftLineDetected) {
-            leftLineDetected = leftDetectBlackLine();
-          } else if (!rightLineDetected) {
-            rightLineDetected = rightDetectBlackLine();
-          }
+            break;
+          case CORRECTING:
+            leftLineDetected = leftLineDetected || leftSampler.getBlackLine();
+            rightLineDetected = rightLineDetected || rightSampler.getBlackLine();
+
+            if (leftLineDetected && rightLineDetected) {
+              // clear the line
+              leftMotor.rotate(4, true);
+              rightMotor.rotate(4, false);
+
+              // Correct
+              correctXYT();
+
+              navigation.setSpeed(FORWARD_SPEED);
+              navigation.navigationMode = TravelingMode.TRAVELING;
+              resetLineDetection();
+            } else if (leftLineDetected) {
+              rightMotor.forward();
+            } else if (rightLineDetected) {
+              leftMotor.forward();
+            }
+            break;
         }
-        
-        leftPrev = leftSample;
-        rightPrev = rightSample;
+
         readingEnd = System.currentTimeMillis();
         if (readingEnd - readingStart < 60) {
           try {
@@ -149,79 +131,28 @@ public class ColorPoller implements Runnable {
   }
 
   /**
-   * get the reading from the left color sensor
-   * 
-   * @return reading of red light from left sensor
-   */
-  public static float getLeftSample() {
-    return leftSample;
-  }
-
-  /**
-   * get the reading from the right color sensor
-   * 
-   * @return reading of red light from right sensor
-   */
-  public static float getRightSample() {
-    return rightSample;
-  }
-
-  /**
-   * Whether of not the left sensor is detecting a black line
-   * 
-   * @return boolean value indicating that the left sensor has transitioned onto a black line
-   */
-  public static boolean leftDetectBlackLine() {
-    if (leftDer < 0 && Math.abs(leftDer) > SENSOR_DERIVATIVE_THRESHOLD) {
-      navigation.stopTheRobot();
-      Sound.beep();
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
-   * Whether of not the right sensor is detecting a black line
-   * 
-   * @return boolean value indicating that the right sensor has transitioned onto a black line
-   */
-  public static boolean rightDetectBlackLine() {
-    if (rightDer < 0 && Math.abs(rightDer) > SENSOR_DERIVATIVE_THRESHOLD) {
-      navigation.stopTheRobot();
-      Sound.twoBeeps();
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
    * corrects the odometer according to the nearest black line using internal value readings
    */
   public static void correctXYT() {
-    int updatedTheta;
+    int updatedTheta = 0;
     double[] currXYT = odometer.getXYT();
+   
     /*
      * Resets the angle (since we are only driving in cardinal directions, there are only 4 possibilities
      */
-    if (currXYT[2] >= 315 || currXYT[2] < 45) {
-      updatedTheta = 0;
-      odometer.setTheta(updatedTheta);
-      correctY(currXYT, updatedTheta);
-    } else if (currXYT[2] >= 45 && currXYT[2] < 135) {
+    
+    if (currXYT[2] >= 45 && currXYT[2] < 135) {
       updatedTheta = 90;
-      odometer.setTheta(updatedTheta);
-      correctX(currXYT, updatedTheta);
     } else if (currXYT[2] >= 135 && currXYT[2] < 225) {
       updatedTheta = 180;
-      odometer.setTheta(updatedTheta);
-      correctY(currXYT, updatedTheta);
     } else if (currXYT[2] >= 225 && currXYT[2] < 315) {
       updatedTheta = 270;
-      odometer.setTheta(updatedTheta);
-      correctX(currXYT, updatedTheta);
+    } else {
+      updatedTheta = 0;
     }
+
+    odometer.setTheta(updatedTheta);
+    correctX(currXYT[0], updatedTheta);
   }
 
   /**
@@ -232,8 +163,7 @@ public class ColorPoller implements Runnable {
    * @param currXYT
    * @param updatedTheta
    */
-  private static void correctX(double[] currXYT, double updatedTheta) {
-    double currX = currXYT[0];
+  private static void correctX(double currX, double updatedTheta) {
     currX = TILE_SIZE * (Math.round((currX) / TILE_SIZE));
     if (updatedTheta == 90) {
       odometer.setX(currX + Resources.SENSOR_RADIUS);
@@ -260,13 +190,12 @@ public class ColorPoller implements Runnable {
     }
   }
 
-  public static void resetLineDetection() {
-    isCorrecting = false;
+  public void resetLineDetection() {
     leftLineDetected = false;
     rightLineDetected = false;
 
     try {
-      Thread.sleep(1000);
+      Thread.sleep(2000);
     } catch (InterruptedException e) {
     }
   }
@@ -278,11 +207,4 @@ public class ColorPoller implements Runnable {
   public void wake() {
     wait = false;
   }
-
-  public boolean[] getLineDetectionStatus() {
-
-    boolean[] result = {leftLineDetected, rightLineDetected};
-    return result;
-  }
-
 }
---
+++ src/ca/mcgill/ecse211/finalproject/phase2/ColorSampler.java
@@ -0,0 +1,90 @@
+package ca.mcgill.ecse211.finalproject.phase2;
+
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+
+/**
+ * Color Sampler
+ * 
+ * @author elias
+ *
+ */
+public class ColorSampler implements Runnable {
+  /**
+   * SampleProvider
+   */
+  private SampleProvider sampleProvider;
+
+  /**
+   * sample reading from the left color sensor
+   */
+  private float[] sample;
+
+  /**
+   * private float prev
+   */
+  private float prev = 0.0f;
+
+  /**
+   * private float der
+   */
+  private float der = 0.0f;
+
+  /**
+   * A threshold value used to determine a large enough change in sensor measurement
+   */
+  private static final float SENSOR_DERIVATIVE_THRESHOLD = 0.05f; // DO NOT CHANGE ME
+
+  /**
+   * blackLine
+   */
+  private boolean blackLine = false;
+
+  /**
+   * Consturctor from a color sensor
+   * 
+   * @param colorSensor
+   */
+  public ColorSampler(EV3ColorSensor colorSensor) {
+    super();
+    this.sampleProvider = colorSensor.getRedMode();;
+    this.sample = new float[colorSensor.sampleSize()];
+  }
+
+  /**
+   * run method for the two color poller
+   */
+  public void run() {
+    long readingStart, readingEnd;
+
+    while (true) {
+      readingStart = System.currentTimeMillis();
+      sampleProvider.fetchSample(sample, 0);
+      float singleSample = sample[0];
+      der = singleSample - prev;
+      blackLine = (der < 0 && Math.abs(der) > SENSOR_DERIVATIVE_THRESHOLD);
+      readingEnd = System.currentTimeMillis();
+      prev = singleSample;
+
+      readingEnd = System.currentTimeMillis();
+      if (readingEnd - readingStart < 100) {
+        try {
+          Thread.sleep(100 - (readingEnd - readingStart));
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  /**
+   * get derivative
+   */
+  public float getDer() {
+    return der;
+  }
+
+  public boolean getBlackLine() {
+    return blackLine;
+  }
+}
